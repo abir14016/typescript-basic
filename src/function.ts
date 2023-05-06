0b977143201ed9c076c1e55399605bc4fea44287
abir14016 <abir14016@gmail.com>
2023-05-06 19:37:45 +0600

    rest operatir, destructering, spread operators

--- src/function.ts
+++ src/function.ts
@@ -4,7 +4,7 @@ function add(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-const result = add(2, 2);
+// const result = add(2, 2);
 
 // console.log(result);
 
@@ -28,4 +28,36 @@ const person: {
   },
 };
 
-console.log(person);
+// console.log(person);
+
+//default parameters
+
+function add2(num1: number, num2: number = 10): number {
+  //Always use default parameters in the last parameter
+  return num1 + num2;
+}
+const result = add2(2);
+// console.log(result);
+
+//spread operator
+const myFriends = ["a", "b", "c"];
+const newFriends = ["d", "e", "f"];
+myFriends.push(...newFriends);
+// console.log(myFriends);
+
+//rest parameters
+const greetFriends = (...friends: string[]): void =>
+  friends.forEach((friend) => console.log(`Hi ${friend}`));
+greetFriends("Gamvir", "Naveen", "Virat", "Kumble", "Anderson");
+
+//object destructuring
+const bestFriend = {
+  fName: "ABir",
+  age: 30,
+};
+const { fName } = bestFriend;
+
+//array destructuring
+const cars = ["Saab", "Volvo", "BMW"];
+const [car, ghar, c, d] = cars;
+console.log(car, ghar, c, d);
